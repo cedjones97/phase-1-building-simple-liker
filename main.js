8757a863a443eddda7b5cc4524663fda09c17a61
cedric <cedricj765@gmail.com>
2023-04-23 16:01:09 -0500

    completed lab

--- main.js
+++ main.js
@@ -5,6 +5,40 @@ const FULL_HEART = '♥'
 // Your JavaScript code goes here!
 
 
+const init = () => {
+ const article = document.querySelector('.media-post').addEventListener('submit', (e) => {
+  e.preventDefault()
+ })
+
+
+}
+const modal = document.querySelector('#modal')
+modal.className = 'hidden'
+const glyph = document.querySelector('.like-glyph')
+
+function handleClick() {
+glyph.addEventListener('click', () => {
+   mimicServerCall().then(() => {
+    if(glyph.innerHTML === EMPTY_HEART){
+      glyph.innerHTML = FULL_HEART
+    glyph.className = 'activated-heart'
+    } else{
+      glyph.innerHTML = EMPTY_HEART
+    glyph.className = 'like-glyph'
+    }
+    // if(glyph.className = 'activated-heart'){
+    //   return glyph.innerHTML = EMPTY_HEART
+    // }
+   }).catch((error) => {
+    let h2 = document.querySelector('h2') 
+    h2.innerHTML = error
+    setTimeout( () => {h2.hidden = true}, 3000)
+   })
+   //<input type="hidden">
+})
+}handleClick()
+//
+
 
 
 //------------------------------------------------------------------------------
